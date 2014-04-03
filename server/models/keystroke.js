af066dd54aa4f35f83994a51e2d23781fd4e27bb
Hack Reactor Students <github@hackreactor.com>
2014-04-03 16:46:41 -0700

    Implement adding keystroke data to mongo -- need to parse

--- server/models/keystroke.js
+++ server/models/keystroke.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 var keystrokeSchema = new mongoose.Schema({
   // user id?
   character: String,
-  count: {type: Number, default: 0}
+  count: Number
 });
 
-keystrokeSchema.methods.initialize = function () {
+// keystrokeSchema.methods.initialize = function () {
 
-};
+// };
 
 module.exports = mongoose.model('Keystroke', keystrokeSchema);
