9c67e9ccbf583c5f0cc0bccbc055e3dc2726a580
Willson Mock <willson.mock@gmail.com>
2014-04-05 18:12:11 -0700

    Refactor and add comments to klicks.js

--- server/models/klicks.js
+++ server/models/klicks.js
@@ -1,19 +1,19 @@
 var mongoose = require('mongoose');
 
 var klicksSchema = new mongoose.Schema({
-  url: String,
-  linkUrl: { type: String, default: "" },
-  width: Number,
-  height: Number,
-  description: String,
+  url: String,  // corresponds to the url where the recording happens
+  linkUrl: { type: String, default: '' },  // corresponds to the url which a user will click on and redirect to the recording
+  width: Number,  // width of the window where the recording happens
+  height: Number,  // height of the window where the recording happens
+  description: { type: String, default: '' },
   ticks: [{
     action: String,
-    pageX: Number,
+    pageX: Number,  // x coordinate relative to the entire page
     pageY: Number,
-    clientX: Number,
+    clientX: Number,  // x coordinate relative to the viewport
     clientY: Number,
     timestamp: Date,
-    target: { type: String, default: "" }, // need a default value to be blank
+    target: { type: String, default: '' }, // need a default value to be blank
     charCode: { type: Number, default: -1 }, // applies only for keypress actions
     altKey: { type: Boolean, default: false }, // applies only for keypress actions
     ctrlKey: { type: Boolean, default: false }, // applies only for keypress actions
@@ -21,9 +21,5 @@ var klicksSchema = new mongoose.Schema({
     shiftKey: { type: Boolean, default: false } // applies only for keypress actions
   }]
 });
-
-// keystrokeSchema.methods.initialize = function () {
-
-// };
 
 module.exports = mongoose.model('Klicks', klicksSchema);
