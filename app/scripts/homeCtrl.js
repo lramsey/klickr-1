c4446cadf312d2079aafb2a3d4674700218464f9
jyek <justinyek@gmail.com>
2014-04-08 12:39:18 -0700

    Fixed server

--- app/scripts/homeCtrl.js
+++ app/scripts/homeCtrl.js
@@ -1,6 +1,6 @@
 angular.module('Klickr')
   .controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
-    var server = 'http://jyek.cloudapp.net:3004/klicks';
+    var server = 'http://jy1.cloudapp.net:3000/klicks';
     $scope.links = [];
 
     $scope.getData = function (){
