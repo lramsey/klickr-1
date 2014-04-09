38f48e91f1ed483356de1736d0fabdee8386715f
jyek <justinyek@gmail.com>
2014-04-09 07:54:51 -0700

    Implemented sort features

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -4,6 +4,18 @@ angular.module('klickrApp')
   .controller('GalleryCtrl', function ($scope, KlickService) {
 
     $scope.spinner = false;
+    $scope.kOrder = '-createdAt';
+
+    /* TODO: Sort toggle */
+    $scope.sortToggle = function(param){
+      if (param && param[0] === '-'){
+        return param.substring(1);
+      } else {
+        return '-' + param;
+      }
+    };
+
+    /* Pulls klicks from server  */
     $scope.refresh = function(){
       $scope.spinner = true;
       return KlickService.getKlicks().then(function(klicks){
